8a1d65fd9c29755dc7aaec139a8231d2187fe05a
ivevasiljevic <ive@profico.hr>
2021-04-03 22:48:07 +0200

    Initial commit

---
+++ app/src/main/java/personal/ive/checklistapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package personal.ive.checklistapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
