cdecbcb067064df748a92c251d2db64aeb0f2c57
ivevasiljevic <ive@profico.hr>
2021-04-07 22:39:43 +0200

    enable flow combinations

--- app/src/main/java/personal/ive/checklistapp/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/personal/ive/checklistapp/ui/tasks/TasksViewModel.kt
@@ -4,6 +4,8 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flatMapLatest
 import personal.ive.checklistapp.data.TaskDao
 
@@ -14,8 +16,15 @@ import personal.ive.checklistapp.data.TaskDao
 class TasksViewModel @ViewModelInject constructor(private val taskDao: TaskDao): ViewModel() {
 
     val searchQuery = MutableStateFlow("")
-    private val tasksFlow = searchQuery.flatMapLatest {
-        taskDao.getTasks(it)
+    val sortOrder = MutableStateFlow(SortTasks.BY_NAME)
+    val isHideCompletedChecked = MutableStateFlow(false)
+
+    private val tasksFlow = combine(searchQuery, sortOrder, isHideCompletedChecked) { searchQuery, sortOrder, isHideCompletedChecked ->
+        Triple(searchQuery, sortOrder, isHideCompletedChecked)
+    }.flatMapLatest { (searchQuery, sortOrder, isHideCompletedChecked) ->
+        taskDao.getTasks(searchQuery, sortOrder, isHideCompletedChecked)
     }
     val tasks = tasksFlow.asLiveData()
 }
+
+enum class SortTasks{ BY_NAME, BY_DATE }
